570d594b01113170abc42a2f8bcd62f1c8a72d82
ameliapham <ptt.huong.8895@gmail.com>
2024-01-03 20:35:49 +0100

    Create quotes table and function randomly shuffle the quotes table

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
-//const backgroundImage = document.querySelector('.container')
-//backgroundImage.style.backgroundImage = "url('/models/library.jpeg')"
-//console.log(backgroundImage)
+let btn = document.querySelector('.button')
+let quote = document.querySelector('.quote')
+let author = document.querySelector('.author')
+
+const quotes = [{
+    quote: '"I want someone who will look at me the same way I look at chocolate cake."',
+    author: "unknow"
+}, {
+    quote: '"If at first you don\'t succeed, have some cake."',
+    author: "Charlie Mackesy"
+}, {
+    quote: '"Being kind to yourself is one of the greatest kindnesses."',
+    author: "Charlie Mackesy"
+}, {
+    quote: '"Nothing beats kindness. It sits quietly beyond all things."',
+    author: "Charlie Mackesy"
+}, {
+    quote: '"If you look at what you have in life, you\'ll always have more. If you look at what you don\'t have in life, you\'ll never have enough."',
+    author: "Oprah Winfrey"
+},{
+    quote: '"My little happiness is a free delicious bubble tea."',
+    author: "Huong"
+},{
+    quote: '"You are the best! And yes you really are."',
+    author: "Joseph"
+},]
+
+btn.addEventListener('click', () => {
+    let random = Math.floor(Math.random() * quotes.length)
+    quote.innerText = quotes[random].quote
+    author.innerText= quotes[random].author
+})
